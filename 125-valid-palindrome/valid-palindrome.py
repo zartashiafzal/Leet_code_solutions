8ccb03ddf9925ce25a2b51eccb1735a67ac57aad
zartashiafzal <65966739+zartashiafzal@users.noreply.github.com>
2023-10-17 08:33:12 +0500

    Time: 66 ms (12.13%) | Memory: 17 MB (93.49%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,18 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l, r = 0, len(s) - 1
-        while l < r:
-            while l < r and not self.alphanum(s[l]):
-                l += 1
+        l, r = 0, len(s) - 1   # Initializing left & right pointers
+        while l < r:           # While left pointer is less than right pointer
+            while l < r and not self.alphanum(s[l]): 
+                l += 1         # Increment right pointer, if not alphanumeric
             while l < r and not self.alphanum(s[r]):
-                r -= 1
-            if s[l].lower() != s[r].lower():
+                r -= 1         # Decrement right pointer, if not alphanumeric
+# If alphanumeric then compare below after making sure each characteris a #lower case and left and right characters are not equal then return false
+            if s[l].lower() != s[r].lower(): 
                 return False
-            l += 1
-            r -= 1
-        return True
+            l += 1              # Update left pointer, increment to right
+            r -= 1              # Update right pointer, decrement to left
+        return True             # If paindrome return TRUE
 
-    # Could write own alpha-numeric function
+    # Create own alpha-numeric function to check if each character passing #from above code is alphanumeic or not (before actually doing the comparison)
     def alphanum(self, c):
         return (
             ord("A") <= ord(c) <= ord("Z")
