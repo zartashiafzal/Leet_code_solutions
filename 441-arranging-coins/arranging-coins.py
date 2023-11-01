e1ebc07fcce75cfbe6f1aad0315c8af23037a67d
zartashiafzal <65966739+zartashiafzal@users.noreply.github.com>
2023-11-01 08:20:38 +0500

    Time: 32 ms (97.59%) | Memory: 16.2 MB (67.91%) - LeetSync

--- 441-arranging-coins/arranging-coins.py
+++ 441-arranging-coins/arranging-coins.py
@@ -1,25 +1,16 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        # Initialize left and right pointers for binary search
-        l, r = 1, n
-        # Initialize the result variable to store the final number of full steps
-        res = 0
-        
-        # Binary search loop
-        while l <= r:
-            # Calculate the middle value of the current search space
-            mid = (l + r) // 2
-            # Calculate the total number of coins that can be arranged using 'mid' steps
-            coins = (mid / 2) * (mid + 1)
-            
-            # Compare the total coins with the given value 'n'
+        l,r= 1,n
+        rows=0
+
+    #binary search
+        while l<=r:
+            mid=(l+r)//2
+            coins=(mid/2)* (mid+1)
+
             if coins > n:
-                # If the total coins exceed 'n', reduce the search space (move to the left)
-                r = mid - 1
+                r=mid-1
             else:
-                # If the total coins do not exceed 'n', update the result and expand the search space (move to the right)
-                l = mid + 1
-                res = max(mid, res)  # Update the result with the current number of steps
-        
-        # Return the maximum number of full steps that can be arranged within 'n' coins
-        return res
+                l=mid+1
+                rows=max(mid,rows)
+        return rows
