f2a71768d0a1271bb3989edd159146b71d455634
zartashiafzal <65966739+zartashiafzal@users.noreply.github.com>
2023-11-02 08:50:58 +0500

    Time: 39 ms (54.67%) | Memory: 16.2 MB (68.63%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.py
+++ 367-valid-perfect-square/valid-perfect-square.py
@@ -1,14 +1,14 @@
-#Brute Force
-class Solution:
-    def isPerfectSquare(self, num: int) -> bool:
-        for i in range(1,num+1):
-            if i*i ==num:
-                return True
-            if i*i> num:
-                return False
+# #Brute Force
+# class Solution:
+#     def isPerfectSquare(self, num: int) -> bool:
+#         for i in range(1,num+1):
+#             if i*i ==num:
+#                 return True
+#             if i*i> num:
+#                 return False
  
 
-#___________ BEST METHOD_____________
+#___________ optimized METHOD_____________
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
         if num ==1:
@@ -26,15 +26,7 @@ class Solution:
                 r=mid-1
         return False
 
-
-
-
-
-
-
-
-
-#___________Method using mid's square___________
+## ___________Method using mid's square___________
 
 #   def isPerfectSquare_2(self, num: int) -> bool:
 #         l ,r = 1, num
