8f142ec798c1ceac27beb4dc3cf07ff47247fda8
zartashiafzal <65966739+zartashiafzal@users.noreply.github.com>
2023-11-02 08:09:37 +0500

    Time: 45 ms (20.69%) | Memory: 16.3 MB (35.45%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.py
+++ 367-valid-perfect-square/valid-perfect-square.py
@@ -1,18 +1,49 @@
+#Brute Force
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        if num == 1:
-            return True
-        
-        low = 1
-        high = num // 2
-        
-        while low <= high:
-            mid = low + (high - low) // 2
-            if mid == num // mid and num % mid == 0:
+        for i in range(1,num+1):
+            if i*i ==num:
                 return True
-            elif mid < num // mid:
-                low = mid + 1
+            if i*i> num:
+                return False
+ 
+
+#___________ BEST METHOD_____________
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        if num ==1:
+            return True
+        l=1
+        r= num//2
+
+        while l<=r:
+            mid=l+(r-l)//2
+            if mid ==num//mid and num%mid==0:
+                return True
+            elif mid < num//mid:
+                l=mid+1
             else:
-                high = mid - 1
-        
+                r=mid-1
         return False
+
+
+
+
+
+
+
+
+
+#___________Method using mid's square___________
+
+#   def isPerfectSquare_2(self, num: int) -> bool:
+#         l ,r = 1, num
+#         while l <= r:
+#             mid = (l +r) // 2
+#             if mid * mid > num:
+#                 r = mid - 1
+#             elif mid * mid < num:
+#                 l = mid + 1
+#             else:
+#                 return True
+#         return False
