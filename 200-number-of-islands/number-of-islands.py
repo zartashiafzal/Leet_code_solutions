1bdb63271d36311294efd7963193a529e1bd0c51
zartashiafzal <65966739+zartashiafzal@users.noreply.github.com>
2023-12-01 06:21:27 +0500

    Time: 279 ms (72.48%) | Memory: 19 MB (57.23%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,44 +1,76 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # Check if the grid is empty
-        if not grid or not grid[0]:
-            return 0
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfsFill(grid, i, j)
+                    count += 1
+        return count
 
-        # Initialize variables
-        islands = 0
-        visit = set()
-        rows, cols = len(grid), len(grid[0])
+    def dfsFill(self, grid, i, j):
+        if 0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == '1':
+            grid[i][j] = '0'
+            self.dfsFill(grid, i + 1, j)
+            self.dfsFill(grid, i - 1, j)
+            self.dfsFill(grid, i, j + 1)
+            self.dfsFill(grid, i, j - 1)
 
-        def dfs(r, c):
-            # Base cases for DFS recursion
-            if (
-                r not in range(rows)
-                or c not in range(cols)
-                or grid[r][c] == "0"
-                or (r, c) in visit
-            ):
-                return
 
-            # Mark the current cell as visited
-            visit.add((r, c))
 
-            # Define directions (right, left, down, up)
-            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
-            # Explore neighbors recursively
-            for dr, dc in directions:
-                dfs(r + dr, c + dc)
+# class Solution:
+#     def numIslands(self, grid: List[List[str]]) -> int:
+#         # Check if the grid is empty
+#         if not grid or not grid[0]:
+#             return 0
 
-        # Iterate through each cell in the grid
-        for r in range(rows):
-            for c in range(cols):
-                # Check if the cell is land and not visited
-                if grid[r][c] == "1" and (r, c) not in visit:
-                    # Start a new island and perform DFS to mark connected land cells
-                    islands += 1
-                    dfs(r, c)
+#         # Initialize variables
+#         islands = 0
+#         visit = set()
+#         rows, cols = len(grid), len(grid[0])
 
-        return islands
+#         def dfs(r, c):
+#             # Base cases for DFS recursion
+#             if (
+#                 r not in range(rows)
+#                 or c not in range(cols)
+#                 or grid[r][c] == "0"
+#                 or (r, c) in visit
+#             ):
+#                 return
+
+#             # Mark the current cell as visited
+#             visit.add((r, c))
+
+#             # Define directions (right, left, down, up)
+#             directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+#             # Explore neighbors recursively
+#             for dr, dc in directions:
+#                 dfs(r + dr, c + dc)
+
+#         # Iterate through each cell in the grid
+#         for r in range(rows):
+#             for c in range(cols):
+#                 # Check if the cell is land and not visited
+#                 if grid[r][c] == "1" and (r, c) not in visit:
+#                     # Start a new island and perform DFS to mark connected land cells
+#                     islands += 1
+#                     dfs(r, c)
+
+#         return islands
+
+
+
+
+
+
+
+
+
+
+
 
 
 # BFS Version
@@ -67,7 +99,7 @@ class SolutionBFS:
                 for dr, dc in directions:
                     nr, nc = row + dr, col + dc
                     # Check if the neighbor is within bounds, is land, and not visited
-                    if (nr) in range(rows) and (nc) in range(cols) and grid[nr][nc] == '1' and (nr, nc) not in visited:
+                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == '1' and (nr, nc) not in visited:
                         # Enqueue the neighbor and mark it as visited
                         q.append((nr, nc))
                         visited.add((nr, nc))
